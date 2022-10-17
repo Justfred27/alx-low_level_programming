2a89cc6a84c1532af73e0a000acc447cb8891031
justfred27 <fredrickduker@gmail.com>
2022-10-17 07:04:44 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
- *main - Entry point 
+ * main -Print a line of codes using puts
  *
- * Return : Always 0 (Success)
- *
- */
+ * Return: 0
+*/
 
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 
 }
