de95a711cf2f0b8ceb3eded65f128783f14de2e8
justfred27 <fredrickduker@gmail.com>
2022-10-17 02:01:27 -0700

    task 3-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point 
+ *
+ * Return : Always 0 (Success)
+ *
+ */
+int main(void)
+{	
+	puts("\"programming is like building a  multi lingual puzzle");
+	return (0);
+
+}
