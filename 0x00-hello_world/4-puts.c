3b9d513e91178523246e8c2e48f9605bc0d49e04
justfred27 <fredrickduker@gmail.com>
2022-10-17 02:25:14 -0700

    task 4-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,10 @@
  * Return : Always 0 (Success)
  *
  */
+
 int main(void)
 {	
-	puts("\"programming is like building a  multi lingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
